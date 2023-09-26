addbe930947cc451393ab725a7bebd809ee2a5e9
kenmaro3 <miha.ken.19@gmail.com>
2023-09-26 09:07:40 +0900

    Initial Commit

---
+++ AuthblueClient/AuthblueClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthblueClientApp.swift
+//  AuthblueClient
+//
+//  Created by Kentaro Mihara on 2023/09/26.
+//
+
+import SwiftUI
+
+@main
+struct AuthblueClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
